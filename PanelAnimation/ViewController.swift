97c393e10f318b04ee8a8caec35ca6d1e0b4ae68
Martin Lloyd <martin.lloyd@thomsonreuters.com>
2016-10-06 11:17:45 +0100

    Initial Commit

---
+++ PanelAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PanelAnimation
+//
+//  Created by Martin Lloyd on 06/10/2016.
+//  Copyright © 2016 Martin Lloyd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
